2a5a6c7b97f245ba84a5f26b815057b4276c24d8
Lindolfo Meira <meiraa@gmail.com>
2025-08-16 16:42:40 -0300

    Added the buffer as a source for completion suggestions.

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -76,7 +76,7 @@ return {
       },
 
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev' },
+        default = { 'lsp', 'path', 'buffer', 'snippets', 'lazydev' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
         },
